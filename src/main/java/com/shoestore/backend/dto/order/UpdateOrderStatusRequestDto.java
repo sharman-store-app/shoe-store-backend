ffb6e9cf2697ad4127696acc5b208c5f20d7df07
Wojtek <wojka66@gmail.com>
2026-07-03 15:18:26 +0200

    fix order status update.

--- src/main/java/com/shoestore/backend/dto/order/UpdateOrderStatusRequestDto.java
+++ src/main/java/com/shoestore/backend/dto/order/UpdateOrderStatusRequestDto.java
@@ -4,7 +4,6 @@ import com.shoestore.backend.model.OrderStatus;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateOrderStatusRequestDto(
-        @NotNull
-        OrderStatus status
+        @NotNull OrderStatus status
 ) {
 }
